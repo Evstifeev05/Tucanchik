45e89c2eeb7eb58375d5019f6124c4600bac4d0a
Evstifeev05 <vavaka016@gmail.com>
2024-07-01 00:09:55 +0300

    Add files via upload

---
+++ Deck.py
@@ -0,0 +1,26 @@
+import random
+from Card import Card
+
+
+class Deck:
+    def __init__(self, cards=None):
+        if cards is None:
+            cards = []
+            for key in Card.CARD_COUNTS:
+                for i in range(Card.CARD_COUNTS[key]):
+                    cards.append(Card(key))
+        self.cards = cards
+
+    def draw_card(self):
+        if self.cards:
+            return self.cards.pop()
+        return None
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def remaining_cards(self):
+        return len(self.cards)
+
+    def save(self):
+        return ' '.join(map(str, self.cards))
