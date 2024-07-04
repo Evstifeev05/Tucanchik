83fea7aec03ea908f698888e90a6967a3afadf23
Evstifeev05 <vavaka016@gmail.com>
2024-07-04 09:40:54 +0300

    Add files via upload

--- HandTest.py
+++ HandTest.py
@@ -1,5 +1,6 @@
 import unittest
 
+from Card import Card
 from Hand import Hand
 
 
@@ -8,9 +9,14 @@ class HandTest(unittest.TestCase):
         self.hand = Hand()
 
     def test_add_cards(self):
-        cards = ['banana', 'fig', 'avocado']
+        cards = [Card('banana'), Card('fig'), Card('avocado')]
         self.hand.add_cards(cards)
         self.assertEqual(self.hand.cards, cards)
+
+    def test_score(self):
+        cards = [Card('acai'), Card('fig'), Card('fig'), Card('fig')]
+        self.hand.add_cards(cards)
+        self.assertEqual(self.hand.score(), 10)
 
 
 if __name__ == '__main__':
